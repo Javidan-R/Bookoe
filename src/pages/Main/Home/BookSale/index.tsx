989c318c5a71220ba82c2b6f7dcf54fb889b88f7
Cavidan Rəcəbli <rajablijavidan@gmail.com>
2023-11-20 23:21:33 +0400

    Update

--- src/pages/Main/Home/BookSale/index.tsx
+++ src/pages/Main/Home/BookSale/index.tsx
@@ -2,20 +2,6 @@ import React, { FC, useState } from 'react'
 import { BookSaleComponent } from '../../../../components/BookSaleComponent'
 
 export const BookSale: FC = () => {
-  const [step, setStep] = useState(1);
-  const [count, setCount] = useState(0);
-  const date = new Date("18 november 2023")
-  date.setDate(date.getDate() + count)
-  
-  function handleReset() {
-    setCount(0);
-    setStep(1);
-  }
-
-  // const handleBonus = () => setStep((s) => s + 1)
-  // const handleMinus = () => setStep((s) => s - 1)
-  // const handleMinusCount = () => step ? setCount((s) => s - step) : " count yoxdu"
-  // const handleBonusCount = () => step ? setCount((s) => s + step) : " count yoxdu"
 
   return (
     <section className='pt-10 m-auto'>
@@ -30,39 +16,7 @@ export const BookSale: FC = () => {
       </div>
 
     
-    <div>
-      <form onSubmit={handleReset}>
-        <input 
-        type="range" 
-        value={step} 
-        onChange={()=>{}} />
-        <div>
-        <button>-</button>
-          <input 
-          type="text" 
-          value={count}
-          onChange={(e)=>{
-            
-
-          }}
-           /> 
-        <button>+</button>  
-        </div>
-        <div>
-          <button>reset</button>
-        </div>
-      </form>
-    </div>
-      <div>
-        <span>{count===0 
-        ? "Today is "
-        : count>0
-        ? `${count} days from today is `
-        : `${Math.abs(count)} days ago was `
-      }</span>
-        {count} days from today  {date.toDateString()}
-      </div>
-
+   
     </section>
 
   )
