7794b34bbfdcb589809c28e4d61d5b04b1ca14cf
Javidan-R <rajablijavidan@gmail.com>
2023-12-07 23:45:50 +0400

    Update index.tsx

--- src/components/PopItem/index.tsx
+++ src/components/PopItem/index.tsx
@@ -1,12 +1,11 @@
 import React, { FC } from 'react'
 interface PopItemProps {
-  image: string
-
+  image: string;
 }
 
 export const PopItem: FC<PopItemProps> = ({ image }) => {
   return (
-    <div className="rounded-lg overflow-hidden bg-gray-400 border-4 border-solid border-white  w-[8.5rem] h-[13rem] relative m-auto ">
+    <div className="rounded-lg overflow-hidden bg-gray-400 border-4 border-solid border-white  w-[8.5rem] h-[13rem]  m-auto ">
       <img
         src={image}
         alt={image}
